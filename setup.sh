0965c977b77e8f694940afab0a5e5eda3e33dcd9
BLACheat78 <37225106+BLACheat78@users.noreply.github.com>
2020-12-06 16:57:50 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#BASH ENCRYPT
+#https://github.com/BLACheat78
+apt update && apt upgrade
+pkg install nodejs
+npm install -g bash-obfuscate
+pkg install cl
+pkg install vim
+pkg install ruby
+gem install lolcat
+pkg install cowsay
+pkg install toilet
+pkg install neofetch
+pkg install nano
